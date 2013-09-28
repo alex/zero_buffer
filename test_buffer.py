c701458840c38ae2898007fb043350b2c9e75ef8
Alex Gaynor <alex.gaynor@gmail.com>
2013-09-28 14:35:27 -0700

    Use buf consistently

--- test_buffer.py
+++ test_buffer.py
@@ -10,11 +10,6 @@ def buf():
     return Buffer.alloc(16)
 
 
-@pytest.fixture
-def builder(request, buf):
-    return buf
-
-
 class TestBuffer(object):
     def test_read_from(self, buf, tmpdir):
         t = tmpdir.join("t.txt")
@@ -98,42 +93,42 @@ class TestBuffer(object):
 
 
 class TestBufferView(object):
-    def test_equality(self, builder):
-        builder.add_bytes(b"abc")
-        assert builder.view() == builder.view()
-        assert builder.view() != builder.view(0, 2)
-        assert not (builder.view() == builder.view(0, 2))
-        assert not (builder.view(0, 2) == builder.view(1, 3))
-        assert not (builder.view(0, 2) != builder.view(0, 2))
+    def test_equality(self, buf):
+        buf.add_bytes(b"abc")
+        assert buf.view() == buf.view()
+        assert buf.view() != buf.view(0, 2)
+        assert not (buf.view() == buf.view(0, 2))
+        assert not (buf.view(0, 2) == buf.view(1, 3))
+        assert not (buf.view(0, 2) != buf.view(0, 2))
 
-    def test_equality_bytes(self, builder):
-        builder.add_bytes(b"abc")
-        assert builder.view() == b"abc"
-        assert builder.view() != b"abd"
-        assert not (builder.view() != b"abc")
-        assert not (builder.view() == b"abd")
-        assert builder.view() != b"ab"
-        assert not (builder.view() == b"ab")
+    def test_equality_bytes(self, buf):
+        buf.add_bytes(b"abc")
+        assert buf.view() == b"abc"
+        assert buf.view() != b"abd"
+        assert not (buf.view() != b"abc")
+        assert not (buf.view() == b"abd")
+        assert buf.view() != b"ab"
+        assert not (buf.view() == b"ab")
 
-    def test_equality_other(self, builder):
-        assert builder.view() != []
+    def test_equality_other(self, buf):
+        assert buf.view() != []
 
-    def test_contains(self, builder):
-        builder.add_bytes(b"abc")
-        view = builder.view()
+    def test_contains(self, buf):
+        buf.add_bytes(b"abc")
+        view = buf.view()
         assert b"a" in view
         assert b"d" not in view
 
-    def test_find_char(self, builder):
-        builder.add_bytes(b"abc")
-        view = builder.view()
+    def test_find_char(self, buf):
+        buf.add_bytes(b"abc")
+        view = buf.view()
         assert view.find(b"a") == 0
         assert view.find(b"c") == 2
         assert view.find(b"d") == -1
 
-    def test_find_char_offsets(self, builder):
-        builder.add_bytes(b"abcdefghijklm")
-        view = builder.view()
+    def test_find_char_offsets(self, buf):
+        buf.add_bytes(b"abcdefghijklm")
+        view = buf.view()
         assert view.find(b"a", 1) == -1
         assert view.find(b"c", 2) == 2
         assert view.find(b"d", 2, 4) == 3
@@ -142,37 +137,37 @@ class TestBufferView(object):
         assert view.find(b"a", -1) == 0
         assert view.find(b"a", 3, 2) == -1
 
-    def test_find_empty_bytes(self, builder):
-        builder.add_bytes(b"abc")
-        view = builder.view()
+    def test_find_empty_bytes(self, buf):
+        buf.add_bytes(b"abc")
+        view = buf.view()
         assert view.find(b"") == 0
         assert view.find(b"", 2) == 2
 
-    def test_find_bytes(self, builder):
-        builder.add_bytes(b"abc123")
-        view = builder.view()
+    def test_find_bytes(self, buf):
+        buf.add_bytes(b"abc123")
+        view = buf.view()
         assert view.find(b"cc") == -1
         assert view.find(b"ab") == 0
         assert view.find(b"c1") == 2
-        builder.add_bytes(b"aabbcc")
-        view = builder.view()
+        buf.add_bytes(b"aabbcc")
+        view = buf.view()
         assert view.find(b"aa") == 6
         assert view.find(b"abb") == 7
 
-    def test_index(self, builder):
-        builder.add_bytes(b"abc123")
-        view = builder.view()
+    def test_index(self, buf):
+        buf.add_bytes(b"abc123")
+        view = buf.view()
         assert view.index(b"b") == 1
         with pytest.raises(ValueError):
             view.index(b"d")
 
-    def test_rfind_empty(self, builder):
-        view = builder.view()
+    def test_rfind_empty(self, buf):
+        view = buf.view()
         assert view.rfind(b"") == 0
 
-    def test_rfind_char(self, builder):
-        builder.add_bytes(b"abc123")
-        view = builder.view()
+    def test_rfind_char(self, buf):
+        buf.add_bytes(b"abc123")
+        view = buf.view()
         assert view.rfind(b"c") == 2
         assert view.rfind(b"3") == 5
         assert view.rfind(b"4") == -1
@@ -180,23 +175,23 @@ class TestBufferView(object):
         assert view.rfind(b"2", 0, 10) == 4
         assert view.rfind(b"2", 10, 0) == -1
 
-    def test_rfind_bytes(self, builder):
-        builder.add_bytes(b"123abc123")
-        view = builder.view()
+    def test_rfind_bytes(self, buf):
+        buf.add_bytes(b"123abc123")
+        view = buf.view()
         assert view.rfind(b"cc") == -1
         assert view.rfind(b"23") == 7
         assert view.rfind(b"124") == -1
 
-    def test_rindex(self, builder):
-        builder.add_bytes(b"abc")
-        view = builder.view()
+    def test_rindex(self, buf):
+        buf.add_bytes(b"abc")
+        view = buf.view()
         assert view.rindex(b"c") == 2
         with pytest.raises(ValueError):
             view.rindex(b"d")
 
-    def test_subscript_slice(self, builder):
-        builder.add_bytes(b"abc123")
-        view = builder.view()
+    def test_subscript_slice(self, buf):
+        buf.add_bytes(b"abc123")
+        view = buf.view()
         assert view[:3] == b"abc"
         assert view[3:] == b"123"
         assert view[2:3] == b"c"
@@ -205,9 +200,9 @@ class TestBufferView(object):
         with pytest.raises(ValueError):
             view[3:2]
 
-    def test_subscript(self, builder):
-        builder.add_bytes(b"abc123")
-        view = builder.view()
+    def test_subscript(self, buf):
+        buf.add_bytes(b"abc123")
+        view = buf.view()
         assert view[0] == ord(b"a")
         assert view[-1] == ord(b"3")
         with pytest.raises(IndexError):
@@ -215,29 +210,29 @@ class TestBufferView(object):
         with pytest.raises(IndexError):
             view[-7]
 
-    def test_split_char(self, builder):
-        builder.add_bytes(b"a-b-c")
-        view = builder.view()
+    def test_split_char(self, buf):
+        buf.add_bytes(b"a-b-c")
+        view = buf.view()
         assert list(view.split(b"-")) == [b"a", b"b", b"c"]
 
-    def test_split_char_maxsplit(self, builder):
-        builder.add_bytes(b"a-b-c")
-        view = builder.view()
+    def test_split_char_maxsplit(self, buf):
+        buf.add_bytes(b"a-b-c")
+        view = buf.view()
         assert list(view.split(b"-", 1)) == [b"a", b"b-c"]
 
-    def test_split_empty(self, builder):
-        view = builder.view()
+    def test_split_empty(self, buf):
+        view = buf.view()
         with pytest.raises(ValueError):
             view.split(b"")
 
-    def test_split_bytes(self, builder):
-        builder.add_bytes(b"a::b::c")
-        view = builder.view()
+    def test_split_bytes(self, buf):
+        buf.add_bytes(b"a::b::c")
+        view = buf.view()
         assert list(view.split(b"::")) == [b"a", b"b", b"c"]
 
-    def test_split_bytes_maxsplit(self, builder):
-        builder.add_bytes(b"a::b::c")
-        view = builder.view()
+    def test_split_bytes_maxsplit(self, buf):
+        buf.add_bytes(b"a::b::c")
+        view = buf.view()
         assert list(view.split(b"::", 1)) == [b"a", b"b::c"]
 
     def test_splitlines(self):
@@ -255,86 +250,86 @@ class TestBufferView(object):
             b"\n", b"abc\n", b"def\r\n", b"ghi\n", b"\r"
         ]
 
-    def test_strip_default_chars(self, builder):
-        builder.add_bytes(b" \t\r\n\f\vabc\t\r\n\f\v ")
-        view = builder.view()
+    def test_strip_default_chars(self, buf):
+        buf.add_bytes(b" \t\r\n\f\vabc\t\r\n\f\v ")
+        view = buf.view()
         assert view.strip() == b"abc"
 
-    def test_strip(self, builder):
-        builder.add_bytes(b"abc123")
-        view = builder.view()
+    def test_strip(self, buf):
+        buf.add_bytes(b"abc123")
+        view = buf.view()
         assert view.strip(b"ab3") == b"c12"
 
-    def test_rstrip_default_chars(self, builder):
-        builder.add_bytes(b" \t\r\n\f\vabc\t\r\n\f\v ")
-        view = builder.view()
+    def test_rstrip_default_chars(self, buf):
+        buf.add_bytes(b" \t\r\n\f\vabc\t\r\n\f\v ")
+        view = buf.view()
         assert view.rstrip() == b" \t\r\n\f\vabc"
 
-    def test_lstrip_default_chars(self, builder):
-        builder.add_bytes(b" \t\r\n\f\vabc\t\r\n\f\v ")
-        view = builder.view()
+    def test_lstrip_default_chars(self, buf):
+        buf.add_bytes(b" \t\r\n\f\vabc\t\r\n\f\v ")
+        view = buf.view()
         assert view.lstrip() == b"abc\t\r\n\f\v "
 
-    def test_lstrip(self, builder):
-        builder.add_bytes(b"abc123")
-        view = builder.view()
+    def test_lstrip(self, buf):
+        buf.add_bytes(b"abc123")
+        view = buf.view()
         assert view.lstrip(b"ab3") == b"c123"
 
-    def test_rstrip(self, builder):
-        builder.add_bytes(b"abc123")
-        view = builder.view()
+    def test_rstrip(self, buf):
+        buf.add_bytes(b"abc123")
+        view = buf.view()
         assert view.rstrip(b"ab3") == b"abc12"
 
-    def test_isspace(self, builder):
-        builder.add_bytes(b"a\t\r\n\f\v ")
-        view = builder.view()
+    def test_isspace(self, buf):
+        buf.add_bytes(b"a\t\r\n\f\v ")
+        view = buf.view()
         assert not view[:1].isspace()
         assert view[1:].isspace()
-        assert not builder.view(0, 0).isspace()
-        assert not builder.view(0, 2).isspace()
+        assert not buf.view(0, 0).isspace()
+        assert not buf.view(0, 2).isspace()
 
-    def test_iteration(self, builder):
-        builder.add_bytes(b"abc")
-        view = builder.view()
+    def test_iteration(self, buf):
+        buf.add_bytes(b"abc")
+        view = buf.view()
         assert list(view) == [ord(b"a"), ord(b"b"), ord(b"c")]
 
-    def test_isdigit(self, builder):
-        builder.add_bytes(b"123abc")
-        assert not builder.view().isdigit()
-        assert builder.view(0, 3).isdigit()
-        assert not builder.view(0, 0).isdigit()
+    def test_isdigit(self, buf):
+        buf.add_bytes(b"123abc")
+        assert not buf.view().isdigit()
+        assert buf.view(0, 3).isdigit()
+        assert not buf.view(0, 0).isdigit()
 
-    def test_isalpha(self, builder):
-        builder.add_bytes(b"abc123")
-        assert not builder.view().isalpha()
-        assert builder.view(0, 3).isalpha()
-        assert not builder.view(0, 0).isalpha()
+    def test_isalpha(self, buf):
+        buf.add_bytes(b"abc123")
+        assert not buf.view().isalpha()
+        assert buf.view(0, 3).isalpha()
+        assert not buf.view(0, 0).isalpha()
 
-    def test_add_contigious(self, builder):
-        builder.add_bytes(b"abc123")
-        assert (builder.view(0, 3) + builder.view(3, 6)) == builder.view()
+    def test_add_contigious(self, buf):
+        buf.add_bytes(b"abc123")
+        assert (buf.view(0, 3) + buf.view(3, 6)) == buf.view()
 
-    def test_add_discontigious(self, builder):
-        builder.add_bytes(b"abc123")
-        bg = builder.view(0, 2) + builder.view(3, 6)
+    def test_add_discontigious(self, buf):
+        buf.add_bytes(b"abc123")
+        bg = buf.view(0, 2) + buf.view(3, 6)
         assert bg == b"ab123"
 
-    def test_add_typeerror(self, builder):
-        view = builder.view()
+    def test_add_typeerror(self, buf):
+        view = buf.view()
         with pytest.raises(TypeError):
             view + 3
 
 
 class TestBufferCollator(object):
-    def test_single_item(self, builder):
-        view = builder.view()
+    def test_single_item(self, buf):
+        view = buf.view()
         collator = BufferCollator()
         collator.append(view)
         assert collator.collapse() is view
 
-    def test_collapse_clears(self, builder):
-        builder.add_bytes(b"abc")
-        view = builder.view()
+    def test_collapse_clears(self, buf):
+        buf.add_bytes(b"abc")
+        view = buf.view()
         collator = BufferCollator()
         collator.append(view)
         collator.collapse()
